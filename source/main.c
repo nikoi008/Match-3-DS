65480c9b4230cb053a6fad356febe9215e713627
Soukie007 <Nmsoukmandjiev007@outlook.com>
2026-02-03 20:00:44 +0000

    Backup

--- source/main.c
+++ source/main.c
@@ -14,6 +14,7 @@
 int grid[GRID_ROWS][GRID_COLS] = {0};
 
 
+
 void initGrid(){
     for(int i = 0; i < GRID_ROWS;i++){
         for(int j = 0; j < GRID_COLS;j++){
@@ -21,70 +22,149 @@ void initGrid(){
         }
     }
 }
+
 void swipeBlocks(int gridX, int gridY, Swipe swipeDir){
-    //if(gridX > 7 || gridX < 0 || gridY > 15 || gridY < 7) return;
+    
+    if(gridX > 7 || gridX < 0 || gridY > 15 || gridY < 7) return;
     
     switch(swipeDir){
         case(SwipeLeft):{ 
-            //if(gridX - 1 < 0) return;
+            if(gridX - 1 < 0) return; 
             
-            int temp = grid[gridX][gridY];
-            grid[gridX][gridY] = grid[gridX -1 ][gridY];
-            grid[gridX - 1][gridY] = temp;
-
+            int temp = grid[gridY][gridX];
+            
+            grid[gridY][gridX] = grid[gridY][gridX - 1]; 
+            grid[gridY][gridX - 1] = temp;
             break;
         }
         case(SwipeRight):{
-            //if(gridX + 1 > 7) return;
-            int temp = grid[gridX][gridY];
-            grid[gridX][gridY] = grid[gridX + 1 ][gridY];
-            grid[gridX + 1][gridY] = temp;
+            if(gridX + 1 > 7) return; 
+            int temp = grid[gridY][gridX];
+            grid[gridY][gridX] = grid[gridY][gridX + 1];
+            grid[gridY][gridX + 1] = temp;
             break;
         }
         case(SwipeUp):{
-            //if(gridY - 1 < 0 ) return;
-            int temp = grid[gridX][gridY];
-            grid[gridX][gridY] = grid[gridX][gridY - 1];
-            grid[gridX][gridY - 1] = temp;
+            if(gridY - 1 < 0 ) return; 
+            
+            int temp = grid[gridY][gridX];
+            grid[gridY][gridX] = grid[gridY - 1][gridX];
+            grid[gridY - 1][gridX] = temp;
             break;
         }
         case(SwipeDown):{
-            //if(gridY + 1 > 15) return;
-            int temp = grid[gridX][gridY];
-            grid[gridX][gridY] = grid[gridX][gridY + 1];
-            grid[gridX][gridY + 1] = temp;
+            if(gridY + 1 > 15) return; 
+            
+            int temp = grid[gridY][gridX]; 
+            grid[gridY][gridX] = grid[gridY + 1][gridX];
+            grid[gridY + 1][gridX] = temp;
             break;
         }
     }
-
 }
 
 
-void drawGridTop(){// top takes[0-7][0-7]
+
+void drawGridTop(){
+    for(int i = 0; i < 8; i++){     
+        for(int j = 0; j < 8; j++){ 
+            
+            int tileValue = grid[j][i]; 
+
+            if(tileValue == -1){
+                NF_SpriteFrame(1,((i * 8) + j) + 64, 0);
+            }else{
+                NF_SpriteFrame(1, ((i * 8) + j) + 64, tileValue + 1);
+            }
+        }
+    }
+}
+bool matchGrid[8][8];
+
+bool findMatches(){
+    bool found = false;
     for(int i = 0; i < 8; i++){
         for(int j = 0; j < 8; j++){
-            if(grid[i][j] == -1){
-                NF_SpriteFrame(1,((i * 8) + j) + 64,0);
-            }else{
-                NF_SpriteFrame(1, ((i * 8) + j) + 64, grid[i][j] + 1);
+            matchGrid[i][j] = false;
+        }
+    }
+    for(int x = 0; x < 8; x++){
+        for(int y = 8; y < 16 - 2; y++){
+        int currentTile = grid[y][x];
+        if(currentTile == grid[y + 1][x] && currentTile == grid[y + 2][x] && currentTile != -1){
+            int matchGridY = y - 8;
+            matchGrid[matchGridY][x] = true;
+            matchGrid[matchGridY + 1][x] = true;
+            matchGrid[matchGridY + 2][x] = true;
+            found = true;
+        }
+        }
+    }
+    for(int y = 8; y < 16; y++){
+        for(int x = 0; x < 8 - 2; x++){
+        int currentTile = grid[y][x];
+        if(currentTile == grid[y][x + 1] && currentTile == grid[y][x + 2] && currentTile != -1){
+            int matchGridY = y - 8;
+            matchGrid[matchGridY][x] = true;
+            matchGrid[matchGridY][x + 1] = true;
+            matchGrid[matchGridY][x + 2] = true;
+            found = true;
+            }
+        }
+    }
+    return found;
+}
+
+
+
+void applyMatches(){
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(matchGrid[i][j] == true){
+                grid[i + 8][j] = -1;
+            }
+        }
+    }
+}
+void applyGravity(){
+    for(int row = 15; row >= 0; row--){
+        for(int col = 7; col >= 0; col--){
+            if(grid[row][col] != -1 && grid[row + 1][col ] == -1){
+                
+                    grid[row + 1][col ] =grid[row][col];
+                    grid[row][col] = -1;
             }
         }
     }
 }
 
-void drawGridBottom(){
+void fillEmptySpaces(){
     for(int i = 0; i < 8; i++){
         for(int j = 0; j < 8; j++){
-            if(grid[i + 8][j] == -1){
-                NF_Set3dSpriteFrame((i* 8)+ j,0);
+            if(grid[i][j] == -1){
+                grid[i][j] = rand() % 6;
             }
-            NF_Set3dSpriteFrame((i * 8) + j, grid[i + 8][j] + 1); 
+        }
+    }
+}
+void drawGridBottom(){
+    for(int i = 0; i < 8; i++){     
+        for(int j = 0; j < 8; j++){
+            
+
+            int tileValue = grid[j + 8][i];
+
+            if(tileValue == -1){
+                NF_Set3dSpriteFrame((i* 8)+ j, 0);   
+            }else{
+                NF_Set3dSpriteFrame((i * 8) + j, tileValue + 1); 
+            }
         }
     }
 }
 
 int main(){
-       uint16_t keys_held;
+    uint16_t keys_held;
     touchPosition touch_pos;
     srand(time(NULL));
     NF_Set2D(0, 0);
@@ -124,20 +204,19 @@ int main(){
     NF_Vram3dSpritePal(0, 0);
     NF_Sort3dSprites();
 
-    //2d sprites
     for(int i = 64; i < 128; i++ ){
-        NF_LoadSpriteGfx("sprite/character", i, 32, 32);//todo change back to 16x16 and use NF_SpriteRotScale  examples/graphics/spriteaffine/
+        NF_LoadSpriteGfx("sprite/character", i, 32, 32);
     }
-    NF_LoadSpritePal("sprite/character", 1);
+    NF_LoadSpritePal("sprite/character", 1); 
     NF_VramSpritePal(1, 1, 1);
-    for(int i = 64; i < 128; i++){
+    for(int i = 64; i < 128; i++){ 
         NF_VramSpriteGfx(1, i, i, true); 
     }
     
     lcdSwap();
     for(int i = 0; i < 8; i++){
         for(int j = 0; j < 8; j++){
-            int x = 38 + ( 20 * i);
+            int x = 38 + ( 20 * i); 
             int y = 16 + ( 20 * j);
             NF_Create3dSprite((i * 8) + j,(i * 8) + j,0,x,y);
             NF_Set3dSpriteFrame((i * 8) + j, grid[i + 8][j]);
@@ -153,33 +232,49 @@ int main(){
             int x = 38 + ( 20 * i);
             int y = 16 + ( 20 * j);
             NF_CreateSprite(1,((i * 8) + j) + 64,((i * 8) + j) + 64,1,x,y);
-            NF_SpriteFrame(1, ((i * 8) + j) + 64, grid[i][j]);
+            NF_SpriteFrame(1, ((i * 8) + j) + 64, grid[j][i]);
         }
     }
     int gridx = 0;
     int gridy = 0;
         char mytext[32] = "";
-    char bufferx[2] = "";
-    char buffery[2] = "";
+    char bufferx[4] = "";
+    char buffery[4] = "";
+    bool touched = false;
+    uint16_t keys_up;
     while(1){
         scanKeys();
         keys_held = keysHeld();
+        keys_up = keysUp();
 
-        if (keys_held & KEY_TOUCH)
+        if (keys_held & KEY_TOUCH && !touched){
                 touchRead(&touch_pos);
-                gridx =   (int)((touch_pos.px - 38)/ 20);
-                gridy = (int)((touch_pos.py - 16)/ 20);
+                gridx = (int)((touch_pos.px - 38)/ 20);
+                gridy = 8 + (int)((touch_pos.py - 16)/ 20);
+                touched = true;
+        }
+        if(keys_up & KEY_TOUCH && touched){
+            touched = false;
+        }
         
         snprintf(bufferx,sizeof(bufferx),"%d",gridx);
         snprintf(buffery,sizeof(buffery),"%d",gridy);
         NF_WriteText(0, 1, 24, 7, bufferx);
         NF_WriteText(0, 1, 24, 8, buffery);
         NF_WriteText(0, 1, 20, 5, mytext);
+
              NF_Draw3dSprites();
- if(getSwipeGesture(SwipeLeft))  { strcpy(mytext, "LEFT  "); swipeBlocks(gridx + 1, gridy, SwipeLeft); }
-    if(getSwipeGesture(SwipeUp))    { strcpy(mytext, "UP"); swipeBlocks( gridx, gridy + 1, SwipeUp); }
-    if(getSwipeGesture(SwipeDown))  { strcpy(mytext, "DOWN  "); swipeBlocks( gridx, gridy - 1, SwipeDown); }
-    if(getSwipeGesture(SwipeRight)) { strcpy(mytext, "RIGHT "); swipeBlocks(gridx - 1, gridy, SwipeRight);}
+ if(getSwipeGesture(SwipeLeft))  { strcpy(mytext, "LEFT  "); swipeBlocks(gridx, gridy, SwipeLeft); }
+    if(getSwipeGesture(SwipeUp))    { strcpy(mytext, "UP"); swipeBlocks( gridx, gridy , SwipeUp); }
+    if(getSwipeGesture(SwipeDown))  { strcpy(mytext, "DOWN  "); swipeBlocks( gridx, gridy, SwipeDown); }
+    if(getSwipeGesture(SwipeRight)) { strcpy(mytext, "RIGHT "); swipeBlocks(gridx, gridy, SwipeRight);}
+        findMatches();
+        applyMatches();
+        applyGravity();
+        for(int i = 0; i < 2;i++) {
+            fillEmptySpaces();
+            applyGravity();
+        }
         drawGridTop();
         drawGridBottom();
         glFlush(0);
@@ -191,4 +286,4 @@ int main(){
 
         NF_Update3dSpritesGfx();
     }
-}
+} 
