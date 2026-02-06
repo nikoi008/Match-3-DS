aef5ca3341807e09765d0ff8f533e9f7f0be4b2e
Soukie007 <Nmsoukmandjiev007@outlook.com>
2026-02-06 20:43:29 +0000

    Added audio, swiping, new sprites
    This shouldve been like 10 commits worth of stuff but i forgor

--- build/Match-3-DS/maxmod/soundbank.h
+++ build/Match-3-DS/maxmod/soundbank.h
@@ -1,6 +1,5 @@
-#define SFX_FIRE_EXPLOSION    0
-#define MOD_LASSE_HAEN_PYYKIT    0
-#define MOD_PARALLAX_80599    1
-#define MSL_NSONGS    2
-#define MSL_NSAMPS    21
-#define MSL_BANKSIZE    23
+#define MOD_GAME    0
+#define SFX_POP    10
+#define MSL_NSONGS    1
+#define MSL_NSAMPS    11
+#define MSL_BANKSIZE    12
